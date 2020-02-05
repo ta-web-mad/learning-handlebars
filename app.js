7ae1effa49e77db12dc1d7409c23c965760b5dc9
f-naranjo <fnaranjo88@gmail.com>
2020-02-05 15:38:15 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,121 @@
+var express = require('express');
+var app = express();
+const path = require('path');
+var hbs = require('hbs');
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+
+
+app.get('/', (req, res, next) => {
+    let data={
+        name: "LA ISLA DE LAS TENTACIONES FAN PAGE",
+        description:"Bienvenido a la web para los enamorados del amor y la fantasía televisiva"
+    }
+    res.render('index', data);
+});
+
+app.get('/singles', (req, res, next) => {
+    const singles = [
+        {
+            'name': 'Lewis',
+            'age': 25,
+            'photo': 'https://www.ecestaticos.com/image/clipping/654/efadca9e4c605f17d6ccea3e04b517b1/lewis-mediaset-espana.jpg'
+        },
+        {
+            'name': 'Óscar',
+            'age': 25,
+            'photo': 'https://www.ecestaticos.com/image/clipping/654/6c0c301b0da5d3dcc7fdd01a55d6f908/oscar-mediaset-espana.jpg'
+        },
+        {
+            'name': 'Rubén',
+            'age': 25,
+            'photo': 'https://www.ecestaticos.com/image/clipping/654/248a0b71230eb93d66acc490c2e6896d/ruben-mediaset-espana.jpg'
+        },
+        {
+            'name': 'Joy',
+            'age': 25,
+            'photo': 'https://www.ecestaticos.com/image/clipping/654/c9ba232abf4a48e4c311be700f89a6ee/joy-mediaset-espana.jpg'
+        },
+        {
+            'name': 'Julian',
+            'age': 25,
+            'photo': 'https://www.ecestaticos.com/image/clipping/654/665c85570ccc2ba53f3f7fd2649082fe/julian-mediaset-espana.jpg'
+        },
+    ]
+    res.render('singles',{singles} );
+});
+
+app.get('/couples', (req, res, next) => {
+    
+const couples = [
+    {
+        'name': 'Alex',
+        'age': '25',
+        'photo': 'https://album.mediaset.es/cimg/1000010/2019/12/17/mila_bb44_-7_b528.jpg',
+        'quote': 'La música es el arte más directo, entra por el oído y va al corazón.'
+    },
+    {
+        'name': 'Fiama',
+        'age': '26',
+        'photo': 'https://album.mediaset.es/cimg/1000010/2019/12/17/mila_bb44_-9_0b75.jpg',
+        'quote': 'Ninguna investigación humana puede ser denominada ciencia si no pasa a través de pruebas matemáticas'
+    },
+    {
+        'name': 'Susana',
+        'age': '30',
+        'photo': 'https://album.mediaset.es/cimg/1000010/2019/12/17/mila_bb44_-10_4fe7.jpg',
+        'quote': 'La magia es tan sólo una extensión de la física. La fantasía son números. Ése es el truco.'
+    },
+    {
+        'name': 'Gonzalo',
+        'age': '32',
+        'photo': 'https://album.mediaset.es/cimg/1000010/2019/12/17/mila_bb44_-7_8cc5.jpg',
+        'quote': 'Desaparecí de tu vida no por gusto ni casualidad, solo para comprobar si me extrañas y me buscas cuando lo hagas '
+    },
+    {
+        'name': 'Adelina',
+        'age': '28',
+        'photo': 'https://album.mediaset.es/cimg/1000010/2019/12/17/mila_bb44_-4_48c7.jpg',
+        'quote': 'El amor no tiene cura, pero es la única cura para todos los males'
+    },
+    {
+        'name': 'Jose',
+        'age': '32',
+        'photo': 'https://album.mediaset.es/cimg/1000010/2019/12/17/mila_bb44_-1_4741.jpg',
+        'quote': 'En un beso, sabrás todo lo que he callado'
+    },
+    {
+        'name': 'Andrea',
+        'age': '24',
+        'photo': 'https://album.mediaset.es/cimg/1000010/2019/12/17/mila_bb44_-5_ccdb.jpg',
+        'quote': 'Más vale pájaro en mano que ciento volando'
+    },
+    {
+        'name': 'Ismael',
+        'age': '22',
+        'photo': 'https://album.mediaset.es/cimg/1000010/2019/12/17/mila_bb44_-8-_-2_8fb3.jpg',
+        'quote': 'Obi, oba, cada día te quiero más'
+    },
+    {
+        'name': 'Estefanía',
+        'age': '34',
+        'photo': 'https://album.mediaset.es/cimg/1000010/2019/12/17/mila_bb44_-3_db5b.jpg',
+        'quote': 'Ya lo noto, ya'
+    },
+    {
+        'name': 'Cristopher',
+        'age': '28',
+        'photo': 'https://album.mediaset.es/cimg/1000010/2019/12/17/mila_bb44_-2_c24a.jpg',
+        'quote': 'Estefaniaaaaaaaa!!!'
+    },
+]
+
+    res.render('couples', {couples});
+});
+
+app.listen(3000);
